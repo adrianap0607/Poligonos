7f6fe27089a000d295eedc304c3d62c77011eddf
adrianap0607 <adrianap2670@gmail.com>
2025-07-13 18:52:25 -0600

    Crear segundo  poligono

--- Fillpoligon/src/main.rs
+++ Fillpoligon/src/main.rs
@@ -8,19 +8,19 @@ fn main() {
 
     let mut fb = Framebuffer::new(width, height, Color::BLACK);
 
-    // Polígono 1
-    let polygon1 = vec![
-        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
-        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383),
+     // Polígono 2
+    let polygon2 = vec![
+        (321, 335), (288, 286), (339, 251), (374, 302),
     ];
-    fb.fill_polygon_scanline(&polygon1, Color::YELLOW);
+    fb.fill_polygon_scanline(&polygon2, Color::BLUE);
     fb.set_current_color(Color::WHITE);
-    for i in 0..polygon1.len() {
-        let (x0, y0) = polygon1[i];
-        let (x1, y1) = polygon1[(i + 1) % polygon1.len()];
+    for i in 0..polygon2.len() {
+        let (x0, y0) = polygon2[i];
+        let (x1, y1) = polygon2[(i + 1) % polygon2.len()];
         fb.draw_line_bresenham(x0, y0, x1, y1);
-
     }
+
+
   // Guardar resultado
     fb.render_to_file("out.png");
     fb.render_to_file("out.bmp");
